d945c4997aa1f0d1999ee71bb88913a08d9527a9
nick <nick121212@126.com>
2016-08-19 14:55:26 +0800

    fx-side-menu指令

--- server/app.js
+++ server/app.js
@@ -1,7 +1,6 @@
 import Koa from 'koa';
 import consign from 'consign';
 import log4js from 'koa-log4';
-import tty from 'tty.js';
 
 const app = new Koa();
 const logger = log4js.getLogger("index");
@@ -20,16 +19,5 @@ consign({
 
 
 app.listen(3000, () => console.log('server started 3000'));
-const ttyApp = tty.createServer({
-    shell: 'bash',
-    users: {
-        foo: 'bar'
-    },
-    port: 7000
-});
-ttyApp.get('/foo', function(req, res, next){
-    res.send('bar');
-});
-ttyApp.listen();
 
 export default app;
---
+++ tty/index.js
@@ -0,0 +1,17 @@
+/**
+ * Created by NICK on 16/8/19.
+ */
+
+const tty = require('tty.js');
+
+const ttyApp = tty.createServer({
+    shell: 'bash',
+    users: {
+        foo: 'bar'
+    },
+    port: 7000
+});
+ttyApp.get('/foo', function (req, res, next) {
+    res.send('bar');
+});
+ttyApp.listen();
