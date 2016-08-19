1dab084b41ec0150c690a5d04e76c3da48a95442
nick <nick121212@126.com>
2016-08-19 14:37:11 +0800

    fx-side-menu指令

--- server/app.js
+++ server/app.js
@@ -3,23 +3,23 @@ import consign from 'consign';
 import log4js from 'koa-log4';
 import tty from 'tty.js';
 
-// const app = new Koa();
-// const logger = log4js.getLogger("index");
-//
-// consign({
-//     extensions: ['.js', '.json', '.node']
-// })
-//     .include('config')
-//     .then('middlewares')
-//     .then('controllers')
-//     .then('routes/passport')
-//     .then('auth')
-//     .then('routes')
-//     .then('controllers')
-//     .into(app, logger);
-//
-//
-// app.listen(3000, () => console.log('server started 3000'));
+const app = new Koa();
+const logger = log4js.getLogger("index");
+
+consign({
+    extensions: ['.js', '.json', '.node']
+})
+    .include('config')
+    .then('middlewares')
+    .then('controllers')
+    .then('routes/passport')
+    .then('auth')
+    .then('routes')
+    .then('controllers')
+    .into(app, logger);
+
+
+app.listen(3000, () => console.log('server started 3000'));
 const ttyApp = tty.createServer({
     shell: 'bash',
     users: {
@@ -32,4 +32,4 @@ ttyApp.get('/foo', function(req, res, next){
 });
 ttyApp.listen();
 
-// export default app;
+export default app;
