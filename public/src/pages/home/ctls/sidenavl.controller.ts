a212680c049a7c00effd3d49fb199ef95260dca2
nick <nick121212@126.com>
2016-09-04 21:50:24 +0800

    action指令修改

--- public/src/pages/home/ctls/sidenavl.controller.ts
+++ public/src/pages/home/ctls/sidenavl.controller.ts
@@ -9,171 +9,14 @@ export class SidenavLeftController {
     $inject = ["mdSideMenuSections", "toolbarUtils", "faAction"];
 
     toolbarBottom: Object;
-    selectedNodes: Dictionary<any>;
+    selectedNodes: Dictionary<any> = [];
     modules: Array<any>;
 
     constructor(private mdSideMenuSections, private toolbarUtils, private fxAction) {
         this.initModules().initToolbar();
     }
 
-    menuLoop(nodes: _.Dictionary<any>, depth: number = 0) {
-        // let nodes = _.filter(nodes, (d) => {
-        //     return d["depth"] === depth;
-        // });
-        //
-        // _.forEach(nodes, (node) => {
-        //     if (nodes[node["parentKey"]]) {
-        //         !nodes[node["parentKey"]].nodes && (nodes[node["parentKey"]].nodes = []);
-        //         // node["menuLink"] = node["link"];
-        //         node["showed"] && nodes[node["parentKey"]].nodes.push(node);
-        //     }
-        // });
-        // nodes.length && this.menuLoop(nodes, depth + 1);
-    }
-
-    /**
-     * 初始化菜单
-     * @returns {SidenavLeftController}
-     */
-    initModules() {
-        this.mdSideMenuSections.sections = [{
-            "menuId": 2,
-            "menuTitle": "系统设置",
-            "icon": "settings",
-            "menuLink": null,
-            "lft": 2,
-            "rgt": 15,
-            "menuParentId": 1,
-            "menuType": 0,
-            "showed": 1,
-            "menuKey": "settings",
-            "columns": null,
-            "depth": 1,
-            "nodes": [
-                {
-                    "menuId": 3,
-                    "menuTitle": "模块管理",
-                    "icon": "view_module",
-                    "menuLink": "#/pages/module",
-                    "lft": 3,
-                    "rgt": 4,
-                    "createAt": 2147483647,
-                    "updateAt": 2147483647,
-                    "menuParentId": 2,
-                    "menuType": 0,
-                    "showed": 1,
-                    "menuKey": "module",
-                    "depth": 2
-                },
-                {
-                    "menuId": 13,
-                    "menuTitle": "接口设置",
-                    "icon": "extension",
-                    "menuLink": "#/pages/interface",
-                    "lft": 11,
-                    "rgt": 12,
-                    "menuParentId": 2,
-                    "menuType": 0,
-                    "showed": 1,
-                    "menuKey": "interface",
-                    "depth": 2
-                },
-                {
-                    "menuId": 14,
-                    "menuTitle": "SCHEMA设置",
-                    "icon": "wallet_giftcard",
-                    "menuLink": "#/pages/schema",
-                    "lft": 13,
-                    "rgt": 14,
-                    "createAt": 2147483647,
-                    "updateAt": 2147483647,
-                    "menuParentId": 2,
-                    "menuType": 0,
-                    "showed": 1,
-                    "menuKey": "schema",
-                    "depth": 2
-                }
-            ]
-        },
-            {
-                "menuId": 5,
-                "menuTitle": "权限设置",
-                "icon": "apps",
-                "menuLink": null,
-                "lft": 16,
-                "rgt": 21,
-                "createAt": 2147483647,
-                "updateAt": 2147483647,
-                "menuParentId": 1,
-                "menuType": 0,
-                "showed": 1,
-                "menuKey": "permiss",
-                "columns": null,
-                "depth": 1,
-                "nodes": [
-                    {
-                        "menuId": 6,
-                        "menuTitle": "权限组管理",
-                        "icon": "group",
-                        "menuLink": "#/pages/group",
-                        "lft": 17,
-                        "rgt": 18,
-                        "menuParentId": 5,
-                        "menuType": 0,
-                        "showed": 1,
-                        "menuKey": "group",
-                        "depth": 2
-                    },
-                    {
-                        "menuId": 7,
-                        "menuTitle": "角色管理",
-                        "icon": "people_outline",
-                        "menuLink": "#/pages/actor",
-                        "lft": 19,
-                        "rgt": 20,
-                        "menuParentId": 5,
-                        "menuType": 0,
-                        "showed": 1,
-                        "menuKey": "actor",
-                        "depth": 2
-                    }
-                ]
-            },
-            {
-                "menuId": 9,
-                "menuTitle": "用户设置",
-                "icon": "person",
-                "menuLink": "",
-                "lft": 22,
-                "rgt": 27,
-                "createAt": 2147483647,
-                "updateAt": 2147483647,
-                "menuParentId": 1,
-                "menuType": 0,
-                "showed": 1,
-                "menuKey": "person_set",
-                "columns": null,
-                "depth": 1,
-                "nodes": [
-                    {
-                        "menuId": 10,
-                        "menuTitle": "用户管理",
-                        "icon": "person_outline",
-                        "menuLink": "#/pages/person",
-                        "lft": 23,
-                        "rgt": 24,
-                        "createAt": 2147483647,
-                        "updateAt": 2147483647,
-                        "menuParentId": 9,
-                        "menuType": 0,
-                        "showed": 1,
-                        "menuKey": "person",
-                        "depth": 2
-                    }
-                ]
-            }
-        ];
-
+    getModules() {
         let promise = this.fxAction.doAction('moduleMenuAction', null);
 
         promise && promise.then((result)=> {
@@ -185,21 +28,55 @@ export class SidenavLeftController {
                 }
             });
 
-            console.log(nodes);
+            let nodesGroupByDepth = _.groupBy(_.keyBy(nodes, "key"), "depth");
+            let depth = 0, root = {};
+
+            while (true) {
+                let nodesIsDepth = nodesGroupByDepth[depth];
+                let parentIsDepth = nodesGroupByDepth[depth - 1];
+
+                if (nodesIsDepth && nodesIsDepth.length > 0) {
+                    switch (depth) {
+                        case 0:
+                            root = nodesIsDepth[0];
+                            break;
+                        case 1:
+                            root['nodes'] = nodesIsDepth;
+                            break;
+                        default:
+                            _.forEach(parentIsDepth, (parentNode)=> {
+                                parentNode["nodes"] = _.filter(nodesIsDepth, (node)=> {
+                                    return node.lft > parentNode.lft && parentNode.rgt > node.rgt;
+                                });
+                            });
+                            break;
+                    }
+                } else {
+                    break;
+                }
+                depth++;
+            }
+
+            this.mdSideMenuSections.sections = root["nodes"];
+            this.modules = this.mdSideMenuSections.sections;
+            this.selectedNodes = nodesGroupByDepth[1] || [];
         });
+    }
+
+    /**
+     * 初始化菜单
+     * @returns {SidenavLeftController}
+     */
+    initModules() {
+        this.getModules();
 
         this.mdSideMenuSections.options = {
             children: "nodes",
-            key: 'menuId',
-            showSearchBar: true,
+            key: 'key',
             dirSelectable: false,
             orderBy: 'lft',
-            filterField: 'menuTitle'
+            filterField: 'key'
         };
-        this.modules = this.mdSideMenuSections.sections;
-        this.selectedNodes = _.keyBy(_.filter(this.modules, (d) => {
-            return d["depth"] === 1;
-        }), "menuId");
 
         return this;
     }
